afbd6bb93660372bfd373b98ba4621f043a42374
kirill <kirias14@gmail.com>
2025-10-07 17:27:50 +0200

    Remove telnet race condition

--- actions/telnet_connector.py
+++ actions/telnet_connector.py
@@ -9,7 +9,7 @@ import telnetlib
 import threading
 import logging
 import time
-from queue import Queue
+from queue import Queue, Empty
 from rich.console import Console
 from rich.progress import Progress, BarColumn, TextColumn, SpinnerColumn
 from shared import SharedData
@@ -112,21 +112,27 @@ class TelnetConnector:
         """
         Worker thread to process items in the queue.
         """
-        while not self.queue.empty():
+        while True:
             if self.shared_data.orchestrator_should_exit:
                 logger.info("Orchestrator exit signal received, stopping worker thread.")
                 break
 
-            adresse_ip, user, password, mac_address, hostname, port = self.queue.get()
-            if self.telnet_connect(adresse_ip, user, password):
-                with self.lock:
-                    self.results.append([mac_address, adresse_ip, hostname, user, password, port])
-                    logger.success(f"Found credentials  IP: {adresse_ip} | User: {user} | Password: {password}")
-                    self.save_results()
-                    self.removeduplicates()
-                    success_flag[0] = True
-            self.queue.task_done()
-            progress.update(task_id, advance=1)
+            try:
+                adresse_ip, user, password, mac_address, hostname, port = self.queue.get(timeout=2)
+            except Empty:
+                break
+
+            try:
+                if self.telnet_connect(adresse_ip, user, password):
+                    with self.lock:
+                        self.results.append([mac_address, adresse_ip, hostname, user, password, port])
+                        logger.success(f"Found credentials  IP: {adresse_ip} | User: {user} | Password: {password}")
+                        self.save_results()
+                        self.removeduplicates()
+                        success_flag[0] = True
+            finally:
+                self.queue.task_done()
+                progress.update(task_id, advance=1)
 
     def run_bruteforce(self, adresse_ip, port):
         self.load_scan_file()  # Reload the scan file to get the latest IPs and ports
